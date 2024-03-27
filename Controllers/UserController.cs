a888402c10a739e97c42ca10d7215e556050e537
Deepak <deepak.kataria13@gmail.com>
2024-03-27 14:57:37 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using DatingApp.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> getUsers()
+        {
+            var users = await _dataContext.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> getUser(int id)
+        {
+            var user = await _dataContext.Users.FindAsync(id);
+            return Ok(user);
+
+
+        }
+    }
+}
---
+++ Entities/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Entities
+{
+    public class AppUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+
+    }
+}
